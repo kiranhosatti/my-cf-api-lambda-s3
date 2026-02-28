2df5f713be499d48d965a1fd5d58fce94afa4717
kiranhosatti <kiranp.hosatti@gmail.com>
2026-02-28 12:25:27 +0530

    first commit

---
+++ lambda/app.py
@@ -0,0 +1,27 @@
+import json
+import urllib.request
+import boto3
+import os
+from datetime import datetime
+
+def lambda_handler(event, context):
+    url = "https://api.github.com"
+
+    response = urllib.request.urlopen(url)
+    data = response.read().decode("utf-8")
+
+    s3 = boto3.client("s3")
+    bucket = os.environ["BUCKET_NAME"]
+
+    filename = f"api-data-{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
+
+    s3.put_object(
+        Bucket=bucket,
+        Key=filename,
+        Body=data
+    )
+
+    return {
+        "statusCode": 200,
+        "body": "Data saved to S3"
+    }
